1dd4baabf855a0305dd7ab3141d74f569aa625e6
shjuly29 <shji9607@gmail.com>
2023-09-13 15:22:47 +0900

    ver1 (230913)

---
+++ js/index.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+  //PC
+  function bigAuto(){
+
+    $(".gallery>ul").stop(true,true).animate({marginLeft:"-=500px"},700,function(){
+      $(".gallery>ul li:first-child").appendTo(".gallery ul");
+      $(this).css({marginLeft:"0px"});
+    });
+  };
+
+  bauto = setInterval(bigAuto,4000);
+
+  $(".gallery").hover(function(){
+    clearInterval(bauto);
+  },function(){
+    setInterval(bigAuto,4000);
+  });
+
+  //mobile
+  $("#gnb_mobile").css({"right":"-60%"});
+
+  qchk = true;
+
+  $(".trigger").click(function(){
+
+    $(this).toggleClass("active");
+    if(qchk){
+      $(this).stop().animate({right:"60%"},500);
+      $("#gnb_mobile").stop().animate({right:"0px"},500);
+      qchk = false;
+    }else{
+      $(this).stop().animate({right:"15px"},500);
+      $("#gnb_mobile").stop().animate({right:"-60%"},500);
+      qchk = true;
+    }
+
+});
+
+});
